a825bf17ace832a0f82f34c53bb70b0af83f584b
ibr <ibr.seckin@gmail.com>
2025-01-09 09:14:28 +0300

    admin user creds from env

--- src/helpers/adminUser.js
+++ src/helpers/adminUser.js
@@ -7,13 +7,11 @@ module.exports = async () => {
   const adminUser = await User.findOne({ isAdmin: true });
   if (!adminUser) {
     await User.create({
-      username: "adminuser", 
-      password: "Aa*12345",
-      fullName: "adminfirst",
-      email: "ibr.seckin@gmail.com",  
-      isAdmin: true, 
-      gender:"male",
-      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6jr5QyaNWzkPClr4Qpln80ceo_HAoVx86IA&s',
+      username: process.env.ADMIN_USER, 
+      password: process.env.ADMIN_PASSWORD,
+      fullName: "admin user",
+      email: process.env.ADMIN_EMAIL,  
+      isAdmin: true,   
     });
     console.log("admin user is added!");
   } else {
